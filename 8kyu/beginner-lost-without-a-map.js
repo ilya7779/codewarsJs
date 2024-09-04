e7aec18a5a0fac04e3d5adbbbbb06a8ba9a568f6
ilya7779 <ilya.makota01@gmail.com>
2024-09-04 09:01:10 +0200

    solved

---
+++ 8kyu/beginner-lost-without-a-map.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function maps(x){
+    return x.map(numb => numb * 2);
+}
+
+// Given an array of integers, return a new array with each value doubled.
+//
+//     For example:
+//
+//     [1, 2, 3] --> [2, 4, 6]
