bf8fe92b38b2ed8c7901b391f91b60f52d48f5de
ilya7779 <ilya.makota01@gmail.com>
2024-09-09 11:47:31 +0200

    Converted line endings to CRLF in workspace.xml

--- 8kyu/convert-a-boolean-to-a-string.js
+++ 8kyu/convert-a-boolean-to-a-string.js
@@ -1,8 +1,15 @@
 
-function booleanToString(b){
-    return String(b);
+function areYouPlayingBanjo(name) {
+    return name[0] === 'r' || name[0] === 'R' ? name + " plays banjo" : name + " does not play banjo";
 }
 
-// Implement a function which convert the given boolean value into its string representation.
+// Create a function which answers the question "Are you playing banjo?".
+//     If your name starts with the letter "R" or lower case "r", you are playing banjo!
 //
-//     Note: Only valid inputs will be given.
+//     The function takes a name as its only argument, and returns one of the following
+// strings:
+//
+//     name + " plays banjo"
+// name + " does not play banjo"
+// Names given are always valid strings.
+
